11613d7d5f9638da54739ef5f2b50d60506ceb8c
Sergio <sergio.orlando013@gmail.com>
2025-11-12 22:39:37 -0300

    Update proyecto_ppot.py

--- proyecto_ppot.py
+++ proyecto_ppot.py
@@ -3,8 +3,25 @@ class juego:
     def papel(self, papel):
         return "hola soy papel"
 
+-------------------------
+import random
+import tkinter as tk
 
+ventana = tk.Tk()
+ventana.geometry("400x300")
+ventana.title("Piedra, Papel o Tijera")
 
+Puntuacion_usuario = 0
+Puntuacion_computadora = 0
+Eleccion_usuario = ""
+Eleccion_computadora = ""
+
+def elije_una_opcion(opcion):
+    opciones = ["Piedra", "Papel", "Tijera"]
+    return opciones[opcion]
+
+def aleatorio_computadora():
+    return random.opcion(["Piedra", "Papel", "Tijera"])
 
 
 
